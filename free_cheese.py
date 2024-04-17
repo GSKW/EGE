9bc955c20c0daf013fd4c49cd4d98a503ea77677
Ilya Grinyuk <bolevard2006@gmail.com>
2024-04-17 21:19:10 +0300

    feat: added our solution for free_cheese 26 task with test

---
+++ free_cheese.py
@@ -0,0 +1,17 @@
+path = '../Downloads/26262626.txt'
+# path = './tests/free_cheese.txt'
+
+# https://inf-ege.sdamgia.ru/problem?id=59851
+
+with open(path, 'r') as file:
+	arr = list(map(int, file.read().splitlines()))
+	N = arr[0]
+	arr = arr[1:]
+n = len(arr) // 3
+free_goods_sum = sum(arr[2::3])
+arr.sort()
+s = sum(arr)
+# print(s - sum(arr[-n:]), s - sum(arr[::3]))
+print(s - sum(arr[:n]), s - free_goods_sum)
+
+
