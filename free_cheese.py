c1824ec01f9c0f6281a3b1dfbb0185d340dc685b
Ilya Grinyuk <bolevard2006@gmail.com>
2024-04-17 21:28:17 +0300

    feat: fixed free_cheese 26 task solution

--- free_cheese.py
+++ free_cheese.py
@@ -11,7 +11,12 @@ n = len(arr) // 3
 free_goods_sum = sum(arr[2::3])
 arr.sort()
 s = sum(arr)
-# print(s - sum(arr[-n:]), s - sum(arr[::3]))
-print(s - sum(arr[:n]), s - free_goods_sum)
+print(s - sum(arr[-n:]), s - sum(arr[-3::-3]))
+# print(s - sum(arr[:n]), s - free_goods_sum)
 
+# 1 2 3 4
+
+# 1 2 3    4
+
+# 1    2 3 4
 
