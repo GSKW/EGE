400029a478419eb09819760d445279f979cc24a2
Ilya Grinyuk <bolevard2006@gmail.com>
2024-05-04 16:05:00 +0300

    feat: updated parity_count task 27

--- parity_count.cpp
+++ parity_count.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <set>
 
 using namespace std;
 typedef long long int ll;
@@ -12,47 +11,45 @@ int main(){
     fstream fin("/Users/IGrinyuk/Downloads/27-B-4.txt");
     ll N; fin >> N;
     ll n_0 = 0, n_1 = 0, s = 0;
-    vector<ll> vec;
+    vector<pair<ll, ll> > vec;
     for(ll i = 0; i < N; i++){
         ll n1, n2; fin >> n1 >> n2;
         ll mn = min(n1, n2);
         s += mn;
         if (mn % 2 == 0){n_0++;}
         else{n_1++;}
-        vec.push_back(abs(n1 - n2));
+        vec.push_back(make_pair(abs(n1 - n2), (mn % 2) - ((n1+n2-mn) % 2)));
     }
-    ll mn1_1 = 1e9;
-    ll mn2_1 = 1e9;
-    for (ll i = 0; i < vec.size(); i++){
-        if (vec[i] % 2 == 1){
-            if (vec[i] < mn1_1){
-                mn2_1 = mn1_1;
-                mn1_1 = vec[i];
+
+    // н -> ч
+    // 1 - 0 = 1
+    ll mn_1 = 1e9;
+    for(ll i = 0; i < N; i++){
+        if (vec[i].second == 1){
+            mn_1 = min(vec[i].first, mn_1);
+        }
+    }
+
+    // ч -> н
+    // ч -> н
+    // 0 - 1 = -1
+    ll mn_0_1 = 1e9;
+    ll mn_0_2 = 1e9;
+    for(ll i = 0; i < N; i++){
+        if (vec[i].second == -1){
+            if(vec[i].first < mn_0_1){
+                mn_0_2 = mn_0_1;
+                mn_0_1 = vec[i].first;
             }
-            else if(vec[i] < mn2_1){
-                mn2_1 = vec[i];
+            else if(vec[i].first < mn_0_2){
+                mn_0_2 = vec[i].first;
             }
         }
     }
 
-    ll mn1_0 = 1e9;
-    ll mn2_0 = 1e9;
-    for (ll i = 0; i < vec.size(); i++){
-        if (vec[i] % 2 == 1){
-            if (vec[i] < mn1_0){
-                mn2_0 = mn1_0;
-                mn1_0 = vec[i];
-            }
-            else if(vec[i] < mn2_0){
-                mn2_0 = vec[i];
-            }
-        }
-    }
-
-    ll s1 = s + mn1_0 + mn2_0 + mn1_1;
-    ll s2 = s + mn1_1 + mn2_1;
-
-//    cout << "All: " << s % 2 << " |0: " << n_0 << " |1: " << n_1 << endl;
+    ll s1 = s + mn_1;
+    ll s2 = s + mn_0_1 + mn_0_2;
     cout << min(s1, s2) << endl;
+
     return 0;
 }
