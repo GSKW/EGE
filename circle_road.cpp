c8d61a7d1a0275ecaf4ecca330fc49185b795bb6
Ilya Grinyuk <bolevard2006@gmail.com>
2024-05-11 14:03:23 +0300

    feat: added fixed solution for circle_road 27 task

--- circle_road.cpp
+++ circle_road.cpp
@@ -4,28 +4,30 @@
 #include <deque>
 
 typedef long long int ll;
-
 using namespace std;
 
+// https://inf-ege.sdamgia.ru/problem?id=45261
+
 int main(){
-    fstream fin("/Users/IGrinyuk/Downloads/107_27_B.txt");
+    fstream fin("/Users/IGrinyuk/Downloads/107_27_A.txt");
     ll N; fin >> N;
     deque<ll> d;
     vector<ll> ans;
     for(ll i = 0; i < N; i++){
         ll num; fin >> num;
-        d.push_back(num);
+        d.push_back(num * 3);
     }
+
     ll n = d.size() / 2 - 1;
     ll s_l = 0, s_r = 0;
     ll s_part_l = 0, s_part_r = 0;
     for(ll i = 0; i < N; i++){
         if (i < n){
-            s_l += d[i] * abs(i - n) * 3;
+            s_l += d[i] * abs(i - n);
             s_part_l += d[i];
         }
         if (i > n){
-            s_r += d[i] * abs(i - n) * 3;
+            s_r += d[i] * abs(i - n);
             s_part_r += d[i];
         }
     }
@@ -36,13 +38,12 @@ int main(){
         d.push_front(last);
         s_l = s_l - s_part_l + last * n;
         s_part_l = s_part_l - d[n] + last;
-        s_r = s_r + s_part_r + d[n + 1] - last*(N-2);
+        s_r = s_r + s_part_r + d[n + 1] - last*(N-n);
         s_part_r = s_part_r - last + d[n + 1];
         ans.push_back(s_l + s_r);
     }
-    // 2 1 0 3 4 5
-    cout << *min_element(ans.begin(), ans.end()) * 3 << endl;
-//    cout << 7 + 8 + 40 + 15 + 26;
+
+    cout << *min_element(ans.begin(), ans.end()) << endl;
 
     return 0;
 }
